80ec401121b19be1085da255fab45cd7603f9607
oromeraa <oromeraa@uoc.edu>
2026-04-01 01:42:16 +0200

    install external dependency

---
+++ src/scripts/typed.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var home_title = new Typed('#home-title', {
+  strings: ['Tradición de sabor', 'Tradición de calidad', 'Tradición de excelencia', 'Tradición de León'],
+  typeSpeed: 100,
+  backSpeed: 0,
+  smartBackspace: true,
+  showCursor: false,
+  loop: false
+});
