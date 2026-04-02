a732adbcc506edfdaf4f2ea4446e4b9791bfb407
oromeraa <oromeraa@uoc.edu>
2026-04-02 06:57:08 +0200

    landing page

--- src/scripts/typed.js
+++ src/scripts/typed.js
@@ -1,9 +1,10 @@
 import Typed from 'typed.js';
 
 var home_typed_title = new Typed('#home-typed-title', {
-  strings: ['Tradición de sabores', 'Tradición de cocina', 'Tradición de lugares', 'Tradición de LEÓN'],
-  typeSpeed: 100,
-  backSpeed: 0,
+  strings: ['Tradición de sabores', 'Tradición de cocina', 'Tradición de lugares', 'Tradición de León'],
+  typeSpeed: 150,
+  backSpeed: 100,
+  backDelay: 1000,
   smartBackspace: true,
   showCursor: false,
   loop: false
