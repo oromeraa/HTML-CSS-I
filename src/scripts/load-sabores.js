7bd7631e9710ffb6d021f139711e30ab0ff7cdbb
oromeraa <oromeraa@uoc.edu>
2026-04-28 01:19:53 +0200

    Direction art done

--- src/scripts/load-sabores.js
+++ src/scripts/load-sabores.js
@@ -5,18 +5,52 @@
 import sabores from '../assets/data-json/sabores.json';
 
 // Solución a eliminar, es una chapuza y me toca hacerlo uno a uno, para esta ocasión lo dejo así pero tengo que buscar la forma de automatizar esto.
-const imgDefault = new URL(`../assets/images/in-construction.jpg?as=avif`, import.meta.url).href;
-const imgChorizo = new URL(`../assets/images/front-chorizo-pradera.jpg?as=avif`, import.meta.url).href;
-const imgCostilla = new URL(`../assets/images/front-costilla-ezequiel.jpg?as=avif`, import.meta.url).href;
+const imgDefault = new URL(`../assets/images/in-construction.jpg`, import.meta.url).href;
+const imgChorizo = new URL(`../assets/images/front-chorizo-pradera.jpg`, import.meta.url).href;
+const imgCostilla = new URL(`../assets/images/front-costilla-ezequiel.jpg`, import.meta.url).href;
 
-console.log('Imagen por defecto cargada:', imgDefault);
-console.log('Imagen del chorizo cargada:', imgChorizo);
-console.log('Imagen de la costilla cargada:', imgCostilla);
+const imgDefaultWebp = new URL(`../assets/images/in-construction.jpg?as=webp`, import.meta.url).href;
+const imgChorizoWebp = new URL(`../assets/images/front-chorizo-pradera.jpg?as=webp`, import.meta.url).href;
+const imgCostillaWebp = new URL(`../assets/images/front-costilla-ezequiel.jpg?as=webp`, import.meta.url).href;
+
+const imgDefaultAvif = new URL(`../assets/images/in-construction.jpg?as=avif`, import.meta.url).href;
+const imgChorizoAvif = new URL(`../assets/images/front-chorizo-pradera.jpg?as=avif`, import.meta.url).href;
+const imgCostillaAvif = new URL(`../assets/images/front-costilla-ezequiel.jpg?as=avif`, import.meta.url).href;
+
+const imgDefaultWebpWide = new URL(`../assets/images/in-construction-wide.jpg?as=webp`, import.meta.url).href;
+const imgChorizoWebpWide = new URL(`../assets/images/front-chorizo-pradera-wide.jpg?as=webp`, import.meta.url).href;
+const imgCostillaWebpWide = new URL(`../assets/images/front-costilla-ezequiel-wide.jpg?as=webp`, import.meta.url).href;
+
+const imgDefaultAvifWide = new URL(`../assets/images/in-construction-wide.jpg?as=avif`, import.meta.url).href;
+const imgChorizoAvifWide = new URL(`../assets/images/front-chorizo-pradera-wide.jpg?as=avif`, import.meta.url).href;
+const imgCostillaAvifWide = new URL(`../assets/images/front-costilla-ezequiel-wide.jpg?as=avif`, import.meta.url).href;
 
 const imagesMap = {
-  'in-construction.jpg': imgDefault,
-  'front-chorizo-pradera.jpg': imgChorizo,
-  'front-costilla-ezequiel.jpg': imgCostilla,
+  default: {
+    'in-construction.jpg': imgDefault,
+    'front-chorizo-pradera.jpg': imgChorizo,
+    'front-costilla-ezequiel.jpg': imgCostilla,
+  },
+  webp: {
+    'in-construction.jpg': imgDefaultWebp,
+    'front-chorizo-pradera.jpg': imgChorizoWebp,
+    'front-costilla-ezequiel.jpg': imgCostillaWebp,
+  },
+  avif: {
+    'in-construction.jpg': imgDefaultAvif,
+    'front-chorizo-pradera.jpg': imgChorizoAvif,
+    'front-costilla-ezequiel.jpg': imgCostillaAvif,
+  },
+  webpWide: {
+    'in-construction.jpg': imgDefaultWebpWide,
+    'front-chorizo-pradera.jpg': imgChorizoWebpWide,
+    'front-costilla-ezequiel.jpg': imgCostillaWebpWide,
+  },
+  avifWide: {
+    'in-construction.jpg': imgDefaultAvifWide,
+    'front-chorizo-pradera.jpg': imgChorizoAvifWide,
+    'front-costilla-ezequiel.jpg': imgCostillaAvifWide,
+  },
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -34,12 +68,29 @@ document.addEventListener('DOMContentLoaded', () => {
   let htmlToInject = '';
 
   sabores.items.forEach((sabor) => {
-    const imageFixed = imagesMap[sabor.src];
-
+    // Solo para obtener los datos de imagen y texto, no es un sabor real
     htmlToInject += `
       <div class="categoria-card">
         <div class="categoria-card-image-wrapper">
-          <img class="categoria-card-image" src="${imageFixed}" alt="${sabor.alt}">
+          <picture>
+            <source media="(max-width: 1023px)"
+              type="image/avif"
+              srcset="${imagesMap['avif'][sabor.src]}">
+            <source
+              type="image/avif"
+              srcset="${imagesMap['avifWide'][sabor.src]}">
+
+            <source media="(max-width: 1023px)"
+              type="image/webp"
+              srcset="${imagesMap['webp'][sabor.src]}">
+            <source
+              type="image/webp"
+              srcset="${imagesMap['webpWide'][sabor.src]}">
+
+            <img class="categoria-card-image" 
+              src="${imagesMap['default'][sabor.src]}" 
+              alt="${sabor.alt}">
+          </picture>
         </div>
         <div class="categoria-card-content">
             <h3 class="categoria-card-title">${sabor.title}</h3>
@@ -59,19 +110,37 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('No se necesitan tarjetas por defecto, ya se han mostrado suficientes items.');
     return;
   } else {
-    const imageFixed = imagesMap[sabores.default.src];
+    const sabor = sabores.default;
 
     const defaultCardHTML = `
       <div class="categoria-card">
         <div class="categoria-card-image-wrapper">
-          <img class="categoria-card-image" src="${imageFixed}" alt="${sabores.default.alt}">
+          <picture>
+            <source media="(max-width: 1023px)"
+              type="image/avif"
+              srcset="${imagesMap['avif'][sabor.src]}">
+            <source
+              type="image/avif"
+              srcset="${imagesMap['avifWide'][sabor.src]}">
+
+            <source media="(max-width: 1023px)"
+              type="image/webp"
+              srcset="${imagesMap['webp'][sabor.src]}">
+            <source
+              type="image/webp"
+              srcset="${imagesMap['webpWide'][sabor.src]}">
+
+            <img class="categoria-card-image" 
+              src="${imagesMap['default'][sabor.src]}" 
+              alt="${sabor.alt}">
+          </picture>
         </div>
         <div class="categoria-card-content">
-          <h3 class="categoria-card-title">${sabores.default.title}</h3>
-          <p class="categoria-card-description">${sabores.default.description}</p>
-          <a class="categoria-card-button" href="${sabores.default.link}">
-            Leer más
-          </a>
+            <h3 class="categoria-card-title">${sabor.title}</h3>
+            <p class="categoria-card-description">${sabor.description}</p>
+            <a class="categoria-card-button" href="${sabor.link}">
+              Leer más
+            </a>
         </div>
       </div>  
     `;
